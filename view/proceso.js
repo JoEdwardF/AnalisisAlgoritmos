09df8cde96ef2b2e24f8e8dbb90012853c5d3bbd
JoEdwardF <105959007+JoEdwardF@users.noreply.github.com>
2022-06-13 19:25:33 -0500

    Add files via upload

--- view/proceso.js
+++ view/proceso.js
@@ -5,6 +5,13 @@ export { array };
 //import {getPuntajeNivel1, getPuntajes} from './visualizar.js'
 var array = new Array();
 
+var promedioPrimeraPregunta = 0;
+var promedioSegundaPregunta = 0;
+var promedioTerceraPregunta = 0;
+var promedioCuartaPregunta = 0;
+var promedioQuintaPregunta = 0;
+
+
 window.addEventListener('DOMContentLoaded', async() => {
     const encuestas = await getRespuestas()
 
@@ -34,17 +41,34 @@ window.addEventListener('DOMContentLoaded', async() => {
             '<td>' + array[i].respuesta3 + '</td>' +
             '<td>' + array[i].respuesta4 + '</td>' +
             '<td>' + array[i].respuesta5 + '</td>';
+
+        if (Number(array[i].respuesta1) != 'NaN') {
+            promedioPrimeraPregunta += Number(array[i].respuesta1);
+        }
+        if (Number(array[i].respuesta2) != 'NaN') {
+            promedioSegundaPregunta += Number(array[i].respuesta2);
+        }
+        if (Number(array[i].respuesta3) != 'NaN') {
+            promedioTerceraPregunta += Number(array[i].respuesta3);
+        }
+        if (Number(array[i].respuesta4) != 'NaN') {
+            promedioCuartaPregunta += Number(array[i].respuesta4);
+        }
+        if (Number(array[i].respuesta5) != 'NaN') {
+            promedioQuintaPregunta += Number(array[i].respuesta5);
+        }
     }
+
+    promedioPrimeraPregunta /= array.length;
+    promedioSegundaPregunta /= array.length;
+    promedioTerceraPregunta /= array.length;
+    promedioCuartaPregunta /= array.length;
+    promedioQuintaPregunta /= array.length;
+
+    document.getElementById("promedioPrimeraPregunta").innerHTML = "Promedio global para la primera pregúnta: " + promedioPrimeraPregunta +
+        "<br/> Promedio global para la segunda pregúnta  : " + promedioSegundaPregunta +
+        "<br/> Promedio global para la tercera pregúnta  : " + promedioTerceraPregunta +
+        "<br/> Promedio global para la cuarta pregúnta  : " + promedioCuartaPregunta +
+        "<br/> Promedio global para la quinta pregúnta  : " + promedioQuintaPregunta;
 
 });
-
-function tabla() {
-    for (var i = 0; i < 10; i++) {
-        con.innerHTML = con.innerHTML +
-            '<td>' + +'</td>' +
-            '<td>Intentos Segundo nivel</td>' +
-            '<td>Intentos Tercer nivel</td>' +
-            '<td>Intentos Cuarto nivel</td>' +
-            '<td>Intentos Quinto nivel</td>';
-    }
-}
